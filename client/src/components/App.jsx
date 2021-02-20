ef65318c0dee69577d75275dc7006351b154b135
robeusanio11 <robeusanio11@gmail.com>
2021-02-19 16:40:20 -0800

    repo setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      messages: [],
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+export default App;
