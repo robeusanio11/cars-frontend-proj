20643fac082ecd1810f4610fe5541d992d86c3e9
robeusanio11 <robeusanio11@gmail.com>
2021-04-08 14:09:19 -0700

    fresh repo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      messages: [],
     }
+
   }
+
 
   render() {
     return (
-      <div>Hello World</div>
+      <>
+        <div>Hello World</div>
+      </>
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => { console.log('Listening on port:', PORT) });
